cc0f1b920ad290927f08bb62dc31851d76868c07
huzaifahadams <huzaifah.adams@gmail.com>
2022-08-08 22:28:18 +0300

    Initial Commit

---
+++ cardWars/cardWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardWarsApp.swift
+//  cardWars
+//
+//  Created by huzaifah Adams on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct cardWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
